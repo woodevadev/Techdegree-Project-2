a46b04f35454b1700c9d54659c37a5bb5e869342
Alan Wood <woodevadev@gmail.com>
2018-05-13 20:22:55 -0400

    Added functions and click event

--- js/also.js
+++ js/also.js
@@ -1,8 +1,118 @@
+//Create listItems object
 const $listItems = $('.student-list').children();
-let listItem;
+//This selects the div that holds the ul's
+//It is selected so that we can append
+//A unordered list at the bottom of the DOM
+const $divToAddTo = $('.student-list').parent();
+//This is the number allowed of students per page
+const numberToShow = 10;
 
-console.log($listItems[0]);
+//This is how we hide things replace the 0 with
+//The index of the one you want to hide
+//$($listItems[0]).hide();
 
-listItem = $($listItems[0]);
+//This hides all students on the page
+$listItems.hide();
 
-listItem.hide();
+//This function determines the number of pages
+//needed to accomdate the list of students based 
+//on the number allowed to be shown
+function numberOfSets(numberOfEntries){
+    let sets = numberOfEntries / numberToShow;
+    sets = Math.ceil(sets);
+    return sets;
+}
+
+//might not need this code
+// //This will return the top index of students
+// //NOT to hide
+// function whichSetsToHide(setOn, numOfSets){
+//     for(let i = 1; i < numOfSets; i ++){
+//         if(setOn == i){
+//             return (i * numberToShow);
+//         }
+//     }
+// }
+
+//Since all of the students are hidden
+//This variabe will show the ones that are supposed
+//to be shown based on the top index
+function showStudents(topIndex){
+    for(let i = (topIndex - 10); i < topIndex; i ++){
+        if($listItems[i]){
+            $($listItems[i]).show();
+        }
+    }
+}
+
+//This adds the outer div and ul to the bottom of the page
+let ulForPageNums = $("<div class='pagination'><ul></ul></div>");
+$divToAddTo.append(ulForPageNums);
+
+//This selects the created ul for appending
+let $ulSelector = $('.pagination ul');
+
+//This function adds the page buttons dynamically 
+function producePageNums(numSets){
+    $ulSelector.append(`
+        <li>
+            <a class="active" href="#">1</a>
+        </li>
+    `);
+    if(numSets > 1){
+        for(let i = 2; i <= numSets; i ++){
+           $ulSelector.append(`
+                <li>
+                    <a href="#">` + i + `</a>
+                </li>
+            `);
+        }
+    }
+}
+
+//Sets the number of sets needed to numSets
+const numberSets = numberOfSets($listItems.length);
+//Populates the page numbers on the bottom
+producePageNums(numberSets);
+
+//Selects all of the list items that were created
+let $liSelector = $('.pagination ul li');
+//Selects all of the a tags in the pagination div
+let $aSelector = $('.pagination ul li a');
+
+$ulSelector.on('click', function(event){
+    //Rehide all list items
+    $listItems.hide();
+
+    //This should remove the class from each list item
+    //Thereby removing the active class
+    $aSelector.each(function(index){
+       $(this).removeClass('active');
+    });
+    //This sets the new active button
+    $(event.target).addClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
